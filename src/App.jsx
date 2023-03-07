877c0ab14b354414061afc7e3fb52f86e1030f49
Michel <michellopez_16@hotmail.com>
2023-03-07 10:12:05 -0600

    Ya se pueden agregar tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import TodoList from "./components/TodoList"
-
-//useState nos permite que nuestros componentes tengan estado
-//El estado es una propiedad que hace que cada cambio del estado
-// renderiza de nuevo el componente
-// hacemos un destructuring con el nombre del estado llamado todos
-// y el que modifica como setTodos
+import { v4 as uuidv4} from "uuid";
+// Añadir el manejador al boton onclick(handleTodoAdd) 
+// UseRef
+// instalamos una libreria  con para geenerar id de manera
+// aleatoria
+// yarn add uuid 
+// lo importamos
 export function App() {
  const [todos,setTodos] = useState([
     {id: 1,task: 'Tarea 1',completed: false}])
  
+    const todoTaskRef = useRef();
+
+    const handleTodoAdd = () =>{
+        const task = todoTaskRef.current.value;
+        if ( task === "") return;
+        // setTodos necesita el estado previo y el nuevo
+        setTodos( (prevTodos) => {
+            return [...prevTodos, {id: uuidv4,task,completed: false}];
+        } )
+    }
+
 return ( 
 <> 
     <div>Hola mundo!!!</div>
     <TodoList todos={todos}/>
-    <input type="text" placeholder="Nueva Tarea"/>
-    <button>➕</button>
+    <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea"/>
+    <button onClick={handleTodoAdd}>➕</button>
     <button>🗑</button>
 </>
  )
- // Jsx solo se puede enviar un elemento antes se englobaba en un div
- // y generaba problemas react saco algo que se le llama fracment
- // 1.- <> </> version reducida
- // 2.- <React.Fragment> <React.Fragment>   
- // 3.- import React, {Fragment, useState} from "react";
-
- // https://es.piliapp.com/emoji/list/ 
- // para añadir emojis
+ 
 }
