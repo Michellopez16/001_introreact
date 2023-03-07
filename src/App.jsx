11ad07880efd36888f0404809558967a21f90ba4
Michel <michellopez_16@hotmail.com>
2023-03-07 09:32:43 -0600

    Genere el primer componente de todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import TodoList from "./components/TodoList"
 
 export function App() {
- return ( <div>Hola mundo!!!</div> )   
+ return ( 
+<>
+    <div>Hola mundo!!!</div>
+    <TodoList todos={[{
+        id: 1,
+        task: 'Tarea 1',
+        completed: false
+    }]}/>
+</>
+ )   
 }
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function TodoItem() {
+  return (
+    <div>TodoItem</div>
+  )
+}
